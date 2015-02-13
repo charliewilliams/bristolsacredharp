a459f4815a304e0e97420351789825a4578c88ed
tterbevets <steve@stitchstitchrecords.co.uk>
2015-02-13 19:42:50 +0000

    Create home.php

---
+++ home.php
@@ -0,0 +1,23 @@
+		<!-- Title -->
+        <div class="row">
+            <div class="col-lg-7">
+            	<img src="img/cw_leading.jpg" class="img-responsive" />
+				<hr />
+                <p><a href="http://en.wikipedia.org/wiki/Sacred_Harp">Sacred Harp</a> is a four-part a capella singing 
+                tradition from the United States. It takes its name from the 
+                <a href="http://en.wikipedia.org/wiki/Shape_note">shape note</a> book that we sing from.</p>
+                <h3>Sing with us</h3>
+                <p>We sing <strong>every Tuesday</strong> from 7&ndash;9pm at St Mark's Baptist Church, Easton, Bristol BS5 6HX.</p>
+                <div class="alert alert-danger" role="alert">
+                	<p><strong>Venue change!</strong> 
+                	From 24 February to 17 March inclusive, we will sing from 7:30&ndash;9:30pm at the Benjamin 
+                	Perry Boathouse.</p>
+                </div>
+                <p><strong>Everyone is welcome.</strong> We are not a choir: no-one is in charge, we don't rehearse,
+                and we don't perform. Everyone who attends sings, loudly and freely, for their own enjoyment.
+                Any week is good for you to begin &mdash; just turn up!</p>
+                <h3>History</h3>
+                <h3>Resources</h3>
+            </div>
+        </div>
+        <!-- /.row -->
