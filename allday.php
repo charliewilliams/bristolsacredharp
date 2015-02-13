3a767a0778bddc3ff5b36594530c823c4ac21be2
tterbevets <steve@stitchstitchrecords.co.uk>
2015-02-13 19:41:29 +0000

    Update and rename allday.html to allday.php

---
+++ allday.php
@@ -0,0 +1,19 @@
+        <!-- Title -->
+        <div class="row">
+            <div class="col-lg-12">
+                <p>The second annual All-Day Singing in Bristol will be held on <strong>Sat 9 May 2015</strong>.</p>
+                <p></p>
+                <h3>Previous minutes</h3>
+                <ul>
+                  <li><a href="http://fasola.org/minutes/search/?n=4440">1st Bristol Singing - 2014</a>. Chair: John Barbour.</li>
+                </ul>
+            </div>
+        </div>
+        <!-- /.row -->
+        <!-- Form -->
+        <div class="row">
+            <div class="col-lg-12">
+                <iframe src="https://docs.google.com/forms/d/1luxucwLPE8U9Fuxdhwjgt1UhV91yglYopDu3yemecgY/viewform?embedded=true" width="760" height="750" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
+            </div>
+        </div>
+        <hr>
