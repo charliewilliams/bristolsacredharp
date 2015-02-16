e4d265a7b419bfa803ceeee8145c5b34668f37a3
tterbevets <steve@stitchstitchrecords.co.uk>
2015-02-16 14:08:46 +0000

    Update allday.php

--- allday.php
+++ allday.php
@@ -2,7 +2,32 @@
         <div class="row">
             <div class="col-lg-12">
                 <p>The second annual All-Day Singing in Bristol will be held on <strong>Sat 9 May 2015</strong>.</p>
-                <p></p>
+                <p>Registration at 10am for a <strong>10:30am start</strong>.</p>
+                
+                <h3>Venue</h3>
+                <address>
+                <strong>Shirehampton Public Hall</strong><br/>
+                Station Road, Shirehampton<br />
+                Bristol, BS11 9TX
+                </address>
+                
+                <h3>Getting here</h3>
+                <h4>By train:</h4>
+                <p>Book a train to <strong>Bristol Temple Meads</strong> (not Bristol Parkway).
+                Booking in advance is strongly recommended for cheaper tickets. There is a connecting service to 
+                <strong>Shirehampton</strong> station, which is a ten minute walk from the venue. 
+                <a href="http://traintimes.org.uk/bristol+temple+meads/shirehampton/10:30a/2015-05-09">Check timetable</a> &mdash; 
+                we recommend the 09:16 service towards Severn Beach.</p>
+                
+                <h4>By coach</h4> 
+                <p>National Express services run to Bristol Bus Station. Take the 40 or 41 bus from Broadmead Union 
+                Street to Shirehampton Green. <a href="http://goo.gl/maps/HnGCT">[map]</a></p>
+                <p>Megabus services run to Colston Street. Take the 40 or 41 bus from The Centre (Ch) 
+                to Shirehampton Green. <a href="http://goo.gl/maps/bm6dj">[map]</a></p>
+                
+                <h4>By car</h4>
+                <p>Leave the M5 at junction 18. There is on-street parking on Station Rd outside the venue.</p>
+                
                 <h3>Previous minutes</h3>
                 <ul>
                   <li><a href="http://fasola.org/minutes/search/?n=4440">1st Bristol Singing - 2014</a>. Chair: John Barbour.</li>
