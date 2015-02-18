36f2a8b4d1fbf707693108654636782cc849ef46
Charlie Williams <c@charliewilliams.org>
2015-02-18 15:47:53 +0000

    Merge

---
+++ gwehelog.php
@@ -0,0 +1,21 @@
+		<!-- Title -->
+        <div class="row">
+            <div class="col-lg-12">  
+                <p>The 16th South Wales Singing in Gwehelog, Usk will be held on <strong>Sat 18 July 2015</strong>.</p>
+                <p></p>
+                <h3>Previous minutes</h3>
+                <ul>
+                  <li><a href="http://fasola.org/minutes/search/?n=4507">15th Gwehelog Singing Weekend - 2014</a>. Chair: Chris Brown.</li>
+                  <li><a href="http://fasola.org/minutes/search/?n=4216">14th Gwehelog Singing Weekend - 2013</a>. Chair: Chris Brown.</li>
+                  <li><a href="http://fasola.org/minutes/search/?n=3933">13th South Wales Singing - 2012</a>. Chair: Chris Brown.</li>
+                  <li><a href="http://fasola.org/minutes/search/?n=3650">12th South Wales Singing - 2011</a>. Chair: Chris Brown.</li>
+                  <li><a href="http://fasola.org/minutes/search/?n=3385">10th South Wales Singing - 2010</a>. Chair: Judy Whiting.</li>
+                  <li><a href="http://www.ukshapenote.org.uk/Gwehelog/GWH2003/gwh2003.htm">5th South Wales Singing - 2003</a>. Chair: Ken Baddley.</li>
+                  <li><a href="http://www.ukshapenote.org.uk/Gwehelog/GWH2002/gwh2002.htm">4th South Wales Singing - 2002</a>. Chair: Ken Baddley.</li>
+                  <li><a href="http://www.ukshapenote.org.uk/Gwehelog/GWH2001/gwh2001-1.htm">3rd South Wales Singing - 2001</a>. Chair: Ken Baddley.</li>
+                  <li><a href="http://www.ukshapenote.org.uk/Gwehelog/GWH2000/gwh2000.htm">2nd South Wales Singing - 2000</a>. Chair: Ken Baddley.</li>
+                  <li><a href="http://www.ukshapenote.org.uk/Gwehelog/GWH1999/gwh1999-1.htm">1st South Wales Singing - 1999</a>. Chair: Ken Baddley.</li>
+                </ul>
+            </div>
+        </div>
+        <!-- /.row -->
