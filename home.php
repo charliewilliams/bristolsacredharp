613ef881e061c926aeb4e035ce51ba37474131aa
tterbevets <steve@stitchstitchrecords.co.uk>
2015-02-20 13:35:02 +0000

    Update home.php

--- home.php
+++ home.php
@@ -2,22 +2,46 @@
         <div class="row">
             <div class="col-lg-7">
             	<img src="img/cw_leading.jpg" class="img-responsive" />
-				<hr />
+		
+		<hr />
+		
                 <p><a href="http://en.wikipedia.org/wiki/Sacred_Harp">Sacred Harp</a> is a four-part a capella singing 
                 tradition from the United States. It takes its name from the 
                 <a href="http://en.wikipedia.org/wiki/Shape_note">shape note</a> book that we sing from.</p>
                 <h3>Sing with us</h3>
-                <p>We sing <strong>every Tuesday</strong> from 7&ndash;9pm at St Mark's Baptist Church, Easton, Bristol BS5 6HX.</p>
+                <p>We sing <strong>every Tuesday</strong> <s>from 7&ndash;9pm at St Mark's Baptist Church, 
+                Easton, Bristol BS5 6HX</s>.</p>
                 <div class="alert alert-danger" role="alert">
                 	<p><strong>Venue change!</strong> 
-                	From 24 February to 17 March inclusive, we will sing from 7:30&ndash;9:30pm at the Benjamin 
-                	Perry Boathouse.</p>
+                	From 24 February to 17 March inclusive, we will sing from 7:30&ndash;9:30pm at 
+                	<address>the <strong>Benjamin Perry Boathouse</strong> Phoenix Wharf, Lower Guinea Street, 
+                	Bristol BS1 6TJ</address>. 
+                	<a href="http://www.avonscouts.org.uk/activity-adventure/campsites-and-activity-centres/benjamin-perry-boathouse/find-the-boathouse/">[directions]</a>
+              		</p>
                 </div>
                 <p><strong>Everyone is welcome.</strong> We are not a choir: no-one is in charge, we don't rehearse,
                 and we don't perform. Everyone who attends sings, loudly and freely, for their own enjoyment.
+                You don't need to be able to read music, although you will start to.
                 Any week is good for you to begin &mdash; just turn up!</p>
-                <h3>History</h3>
+                
+                <!--- <h3>History</h3> 
+                to be filled out.
+                --->
+                
                 <h3>Resources</h3>
+                <p><a href="http://fasola.org/resources/Grayson_Beginners_Guide_2012.pdf">Lisa Grayson's beginners'
+                guide</a> (pdf) is an excellent introduction to how Sacred Harp singing works.</p>
+                <p><a href="http://fasola.org/">fasola.org</a> is the main American website.
+                <a href="http://ukshapenote.org.uk/">UK Shapenote</a> is the main UK site, and has a calendar of all
+                of the regular singings.</p>
+                <p>You can buy a book from us if you come and sing with us. Or you can order one from 
+                <a href="http://originalsacredharp.com/ordering-the-sacred-harp/">the publishing company</a>; 
+                shipping to the UK is subsidised so there is no extra cost.</p>
+                <p>To hear authentic old recordings of Sacred Harp, we recommend the 
+                <a href="http://research.culturalequity.org/rc-b2/get-audio-ix.do?ix=recording&id=352&idType=genreId&sortBy=abc">1959 United Convention</a>
+                and the <a href="http://originalsacredharp.com/museum/national-convention/">1980 First National Convention</a>. The 
+                <a href="http://londonsacredharp.bandcamp.com/album/7th-annual-london-all-day-singing-25-oct-2014">2014 London All-Day Singing</a>
+                is a good example of current UK singing.</p>
             </div>
         </div>
         <!-- /.row -->
