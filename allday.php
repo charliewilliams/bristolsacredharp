b930f40d5dccc0241faf9a11a8f6a9a51844ab1a
tterbevets <steve@stitchstitchrecords.co.uk>
2015-03-31 14:04:31 +0100

    Update allday.php

--- allday.php
+++ allday.php
@@ -1,5 +1,3 @@
-        <!-- Title -->
-        <div class="row">
             <div class="col-lg-8">
                 <p>The second annual All-Day Singing in Bristol will be held on <strong>Sat 9 May 2015</strong>.</p>
                 <p>Registration at 10am for a <strong>10:30am start</strong>.</p>
@@ -28,17 +26,21 @@
                 <h4>By car</h4>
                 <p>Leave the M5 at junction 18. There is on-street parking on Station Rd outside the venue.</p>
                 
+                <h3>Accommodation</h3>
+                <p>If you'd like to request being hosted by local singers, please email
+                <a href="mailto:bristolsacredharp@gmail.com?subject=Accommodation">bristolsacredharp@gmail.com</a>.
+                We'd be very happy to host you!</p>
+                
                 <h3>Previous minutes</h3>
                 <ul>
                   <li><a href="http://fasola.org/minutes/search/?n=4440">1st Bristol Singing - 2014</a>. Chair: John Barbour.</li>
                 </ul>
             </div>
-        </div>
-        <!-- /.row -->
-        <!-- Form -->
+        
+        <!-- Form 
         <div class="row">
             <div class="col-lg-12">
                 <iframe src="https://docs.google.com/forms/d/1luxucwLPE8U9Fuxdhwjgt1UhV91yglYopDu3yemecgY/viewform?embedded=true" width="760" height="750" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
             </div>
-        </div>
+        </div> -->
         <hr>
