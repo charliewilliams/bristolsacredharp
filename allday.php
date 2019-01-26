3be0b78f8c9b2d3dd3eb1fc85669b6683cb475a4
Steve <steve@steves-macbook-pro.local>
2019-01-26 18:40:24 +0000

    Updated with 2019 info

--- allday.php
+++ allday.php
@@ -1,69 +1,74 @@
             	<img src="img/2015allday.jpg" class="img-responsive" />
-		
+
 				<hr />
-				
-                <p>The fourth annual All-Day Singing in Bristol will be held on <strong>Sat 13 May 2017</strong>.</p>
+
+                <p>Our all-day singing is held on the Saturday before the second Sunday in May. <br />
+                The sixth annual all-day singing in Bristol will be held on <strong>Sat 11 May 2019</strong>.</p>
                 <p>Registration at 10am for a <strong>10:30am start</strong>.</p>
-                
+
             	<p>Chair<br />
-            	<strong>Kate Kirwan</strong></p>
-            	
-            	<p>Vice-Chair<br />
-            	<strong>Sam Cole</strong></p>
-                
+            	<strong>Rachel Wemyss</strong></p>
+
+            	<p>Vice-chair<br />
+            	<strong>Alec Williams</strong></p>
+
+
                 <h3>Venue</h3>
                 <address>
                 <strong>Frenchay Quaker Meeting House</strong><br/>
                 Beckspool Road, Frenchay<br />
                 Bristol, BS16 1NT
                 </address>
-                
+
                 <h3>Getting here</h3>
                 <h4>From Bristol city centre</h4>
                 <p>From Bristol Bus Station, take the <a href="https://www.firstgroup.com/uploads/maps/Service_46_X46_WEB.pdf">46 bus</a> (destination Yate) to Frenchay. It stops at the junction of Bristol Rd and Beckspool Rd, a short walk from the venue.</p>
-                
+
                 <h4>By train</h4>
                 <p>Bristol Parkway may be the most convenient station for this singing. Booking in advance is strongly recommended for cheaper tickets. From Parkway, the <a href="https://www.firstgroup.com/uploads/maps/Bristol_Service_18_18A_WEB.pdf">18/18A</a> bus (destination Emersons Green) runs to Bristol Rd, a short walk from the venue.
                 </p>
-                
-                <h4>By coach</h4> 
+
+                <h4>By coach</h4>
                 <p>National Express services run to Bristol Bus Station. Megabus services run to Bond Street, near Cabot Circus. Follow local directions from the city centre as above.</p>
-    			
+
                 <h4>By car</h4>
-                <p>Leave the M4 at junction 19 onto the M32. Take the first exit at junction 1 onto the A4174, then the first right 
+                <p>Leave the M4 at junction 19 onto the M32. Take the first exit at junction 1 onto the A4174, then the first right
                 onto Bristol Rd and then the first left onto Beckspool Rd. There is on-street parking on Beckspool Rd outside the venue.</p>
-                
-                
-                <h3>Friday singing and social</h3>
+
+                <!--
+                <h3>Friday singing</h3>
                 <p>If you're around on Friday, please join us for a composium, singing new compositions in the Sacred Harp style.<br/>
-				Venue to be announced.</p>
-				
-				<!--- <h3>Saturday Social</h3>
-				<p>We will meet for a social after the singing on Saturday, around 7pm, at 
-				<strong>Cafe Kino</strong>, 108 Stokes Croft, Bristol, BS1 3RU.</p> --->
-				
+				This will be from 7&ndash;9pm at our usual weekly singing venue: </p>
+				<p><strong><a href="http://www.avonscouts.org.uk/activity-adventure/campsites-and-activity-centres/benjamin-perry-boathouse/find-the-boathouse/">The Benjamin Perry Boathouse</a></strong>,<br/>
+				Phoenix Wharf, Lower Guinea Street, <br/>
+				Bristol, BS1 6TJ.</p>
+
+				<h3>Saturday social</h3>
+				<p>We will meet for a social at 8pm after the singing on Saturday at: </p>
+				<p><strong><a href="https://thegreenbankbristol.co.uk/">The Greenbank</a></strong>,<br/>
+				57 Bellevue Road,<br/>
+				 Bristol, BS5 6DP.</p>
+				<p>The Greenbank serves a wide variety of wines, ales and craft beers, as well as wood-fired pizza and other meals.</p>
+
+
 				<h3>Sunday singing</h3>
-				<p>Please join us for a Christian Harmony singing and lunch at Bailbrook Sunday School, Bath Spa. 
-				Best reached by car, or a taxi from Bath Spa station. Ask at the singing for more information.</p> 
-			
-                
+				<p>Please join us for a casual Christian Harmony singing and lunch from 2pm at Bailbrook Sunday School, Bath Spa.
+				Best reached by car, or a taxi from Bath Spa station. Ask at the singing for more information.</p>
+			-->
+
                 <h3>Accommodation</h3>
-                <p>Accommodation with local singers will be open to subscription from mid-March.
-                We'd be very happy to host you! For further information, you can email <a href="mailto:bristolsacredharp@gmail.com">bristolsacredharp@gmail.com</a>.</p>
-                
-                
+                <p>Accommodation with local singers will be available soon.
+                  <!--
+                <p>Accommodation is available with local singers. Please use this <a href="">accommodation form</a> to sign up.
+                We'd be very happy to host you! Otherwise, there are many hotel and Airbnb options in Bristol. For further information, you can email <a href="mailto:bristolsacredharp@gmail.com">bristolsacredharp@gmail.com</a>.</p>
+
+              -->
                 <h3>Previous minutes</h3>
                 <ul>
+                  <li><a href="http://fasola.org/minutes/search/?n=5615">5th Bristol Singing &ndash; 2018</a>. Chair: Samantha Cole.</li>              
+                	<li><a href="http://fasola.org/minutes/search/?n=5316">4th Bristol Singing &ndash; 2017</a>. Chair: Kate Kirwan.</li>
                 	<li><a href="http://fasola.org/minutes/search/?n=5017">3rd Bristol Singing &ndash; 2016</a>. Chair: Barry Parsons.</li>
                 	<li><a href="http://fasola.org/minutes/search/?n=4724">2nd Bristol Singing &ndash; 2015</a>. Chair: Steve Brett.</li>
             		<li><a href="http://fasola.org/minutes/search/?n=4440">1st Bristol Singing &ndash; 2014</a>. Chair: John Barbour.</li>
                 </ul>
             <hr />
-
-        
-        <!-- Form 
-        <div class="row">
-            <div class="col-lg-12">
-                <iframe src="https://docs.google.com/forms/d/1luxucwLPE8U9Fuxdhwjgt1UhV91yglYopDu3yemecgY/viewform?embedded=true" width="760" height="750" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
-            </div>
-        </div> -->
