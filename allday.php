46d00485f38c6804d91bee89aef47a46aa928a76
Steve Brett <steve@atomicsmash.co.uk>
2023-03-08 20:29:18 +0000

    2023 all day updates

--- allday.php
+++ allday.php
@@ -3,14 +3,14 @@
 				<hr />
 
                 <p>Our all-day singing is held on the Saturday before the second Sunday in May. <br />
-                The sixth annual all-day singing in Bristol will be held on <strong>Sat 11 May 2019</strong>.</p>
+                The eighth annual all-day singing in Bristol will be held on <strong>Sat 13 May 2023</strong>.</p>
                 <p>Registration at 10am for a <strong>10:30am start</strong>.</p>
 
             	<p>Chair<br />
-            	<strong>Rachel Wemyss</strong></p>
+            	<strong>Bridget McVennon-Morgan</strong></p>
 
             	<p>Vice-chair<br />
-            	<strong>Alec Williams</strong></p>
+            	<strong>Samuel Turner</strong></p>
 
 
                 <h3>Venue</h3>
@@ -36,33 +36,35 @@
                 onto Bristol Rd and then the first left onto Beckspool Rd. There is on-street parking on Beckspool Rd outside the venue.</p>
 
                 <h3>Friday singing</h3>
-                <p>If you're around on Friday, please join us for a composium, singing new compositions in the Sacred Harp style.<br/>
-        				This will be from 7&ndash;9pm at our usual weekly singing venue: </p>
-        				<p><strong><a href="https://goo.gl/maps/ajycuowpbAu">The Benjamin Perry Boathouse</a></strong>,<br/>
-        				Phoenix Wharf, Lower Guinea Street, <br/>
-        				Bristol, BS1 6TJ.</p>
-
-        				<h3>Saturday social</h3>
-        				<p>We will meet for a social at 7pm after the singing on Saturday at: </p>
-        				<p><strong><a href="https://thegreenbankbristol.co.uk/">The Greenbank</a></strong>,<br/>
-        				57 Bellevue Road,<br/>
-        				 Bristol, BS5 6DP.</p>
-        				<p>The Greenbank serves a wide variety of wines, ales and craft beers, as well as wood-fired pizza and other meals.</p>
-
-        				<h3>Sunday singing</h3>
-        				<p>Please join us for a Christian Harmony singing from 2&ndash;4pm at: </p>
+                <p>If you're around on Friday 12th, please join us for a composium, singing new compositions in the Sacred Harp style.<br/>
+                This will be from 7&ndash;9pm at: </p>
                 <p><strong><a href="http://www.stanneschurchbristol.org.uk">St. Anne's Church Hall</a></strong>,<br/>
-        				St Leonards Road,<br/>
-        				Bristol, BS5 6JN.</p>
+                St Leonards Road,<br/>
+                Bristol, BS5 6JN.</p>
+
+                <h3>Saturday social</h3>
+                <p>We will meet for a social at 8pm after the singing on Saturday at: </p>
+                <p><strong><a href="https://thegreenbankbristol.co.uk/">The Greenbank</a></strong>,<br/>
+                57 Bellevue Road,<br/>
+                    Bristol, BS5 6DP.</p>
+                <p>The Greenbank serves a wide variety of wines, ales and craft beers, as well as wood-fired pizza and other meals.</p>
+
+                <h3>Sunday singing</h3>
+                <p>On Sunday 14th, please join us for a Christian Harmony singing from 1&ndash;4pm at: </p>
+                <p><strong><a href="http://www.stanneschurchbristol.org.uk">St. Anne's Church Hall</a></strong>,<br/>
+                St Leonards Road,<br/>
+                Bristol, BS5 6JN.</p>
                 <p>Some loaner books will be available to use.</p>
 
-                <h3>Accommodation</h3>
+                <!-- <h3>Accommodation</h3>
                 <p>Accommodation is available with local singers. Please use this <a href="https://goo.gl/forms/5P89xRc7U3ro8q2z1">accommodation form</a> to sign up.
-                We'd be very happy to host you! Otherwise, there are many hotel and Airbnb options in Bristol. For further information, you can email <a href="mailto:bristolsacredharp@gmail.com">bristolsacredharp@gmail.com</a>.</p>
+                We'd be very happy to host you! Otherwise, there are many hotel and Airbnb options in Bristol. For further information, you can email <a href="mailto:bristolsacredharp@gmail.com">bristolsacredharp@gmail.com</a>.</p> -->
 
                 <h3>Previous minutes</h3>
                 <ul>
-                  <li><a href="http://fasola.org/minutes/search/?n=5615">5th Bristol Singing &ndash; 2018</a>. Chair: Samantha Cole.</li>
+                    <li><a href="http://fasola.org/minutes/search/?n=6369">7th Bristol Singing &ndash; 2022</a>. Chair: Alec Williams.</li>
+                    <li><a href="http://fasola.org/minutes/search/?n=5916">6th Bristol Singing &ndash; 2019</a>. Chair: Rachel Wemyss.</li>
+                    <li><a href="http://fasola.org/minutes/search/?n=5615">5th Bristol Singing &ndash; 2018</a>. Chair: Samantha Cole.</li>
                 	<li><a href="http://fasola.org/minutes/search/?n=5316">4th Bristol Singing &ndash; 2017</a>. Chair: Kate Kirwan.</li>
                 	<li><a href="http://fasola.org/minutes/search/?n=5017">3rd Bristol Singing &ndash; 2016</a>. Chair: Barry Parsons.</li>
                 	<li><a href="http://fasola.org/minutes/search/?n=4724">2nd Bristol Singing &ndash; 2015</a>. Chair: Steve Brett.</li>
